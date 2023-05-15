3fff65f9ecfad85b63b2f43d2a7c144faf4716d3
Emily Wetterauer <emmawetterauer@gmail.com>
2023-05-14 19:19:36 -0600

    baseUrl changed to typicode from localhost

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,8 +1,8 @@
 const token = localStorage.getItem("jwt");
 
-export const baseUrl = "http://localhost:3001";
-// export const baseUrl =
-//   "https://my-json-server.typicode.com/EmilyWetterauer/se_project_react";
+// export const baseUrl = "http://localhost:3001";
+export const baseUrl =
+  "https://my-json-server.typicode.com/EmilyWetterauer/se_project_react";
 
 export const handleServerResponse = (res) => {
   const resp = res.json();
